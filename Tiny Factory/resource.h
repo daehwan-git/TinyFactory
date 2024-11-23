35681a2dedb15f4e8bae8fdb907365a544c3d6e7
lbiotitel <biotite135@naver.com>
2024-11-23 15:41:44 +0900

    2024 11 23

--- Tiny Factory/resource.h
+++ Tiny Factory/resource.h
@@ -56,6 +56,7 @@
 #define ROBOT_DELAY_BTN                 1035
 #define CONVEYOR_CONDITION_TEXT         1036
 #define ROBOTARM_CONDITION_TEXT         1037
+#define IDC_BUTTON7                     1038
 
 // Next default values for new objects
 // 
@@ -63,7 +64,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        141
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1038
+#define _APS_NEXT_CONTROL_VALUE         1039
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
